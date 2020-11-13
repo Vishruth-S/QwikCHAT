3ec6be227b631c9502107d616d1a116b9bdc91b7
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-13 12:23:17 +0530

    Added mode

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Chat from './components/Chat/Chat'
 import Join from './components/Join/Join'
 import Navbar from './components/Navbar/Navbar';
+import './App.css'
 
 const App = () => {
+  const [mode, setMode] = useState("dark");
   return (
     <Router>
-      <Navbar />
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
+      <button id="mode-toggle" onClick={() => setMode(mode === "dark" ? "light" : "dark")}>Mode</button>
+      <Navbar theme={mode} />
+      <Route path="/" exact component={() => <Join theme={mode} />} />
+      <Route path="/chat" component={() => <Chat theme={mode} />} />
     </Router>
   )
 }
