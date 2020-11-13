8bee0ea0431068481d7ebc71747511426236a796
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-13 12:24:44 +0530

    Added dark and light mode for navbar

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './Navbar.css'
 import Logo from '../../Assets/QwikChat.png'
 
-const Navbar = () => {
+const Navbar = ({ theme }) => {
     return (
-        <div className="navbar">
+        theme === "dark" ? (<div className="navbar-light">
             <span className="navbar-brand" ><img src={Logo} className="nav-logo" alt="Infix2Postfix_logo" /></span>
             <span className="nav-right">Made by VS</span>
-        </div>
+        </div>) : (<div className="navbar-dark">
+            <span className="navbar-brand" ><img src={Logo} className="nav-logo" alt="Infix2Postfix_logo" /></span>
+            <span className="nav-right">Made by VS</span>
+        </div>)
+
     )
 }
 
