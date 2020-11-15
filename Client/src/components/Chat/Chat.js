f30c7acf2b4924ba823c6292e6d1e684bc92cdab
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-15 16:51:08 +0530

    netlify config

--- Client/src/components/Chat/Chat.js
+++ Client/src/components/Chat/Chat.js
@@ -19,7 +19,8 @@ const Chat = (props) => {
     const [users, setUsers] = useState('')
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState([])
-    const ENDPOINT = 'localhost:5000'
+    // const ENDPOINT = 'https://qwikchat.herokuapp.com/'
+    const ENDPOINT = 'http://localhost:5000/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search)
