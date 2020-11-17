5e532fd0431af21d32283bf12cdb14fd68861d50
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-17 16:54:07 +0530

    initial working state

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -6,8 +6,10 @@ const Navbar = ({ theme }) => {
     return (
         theme === "dark" ? (<div className="navbar-light">
             <span className="navbar-brand" ><img src={Logo} className="nav-logo" alt="Infix2Postfix_logo" /></span>
+            <div className="vs">Made by <a href="https://vishruth-s.github.io/Me">VS</a></div>
         </div>) : (<div className="navbar-dark">
             <span className="navbar-brand" ><img src={Logo} className="nav-logo" alt="Infix2Postfix_logo" /></span>
+            <div className="vs">Made by <a href="https://vishruth-s.github.io/Me">VS</a></div>
         </div>)
 
     )
