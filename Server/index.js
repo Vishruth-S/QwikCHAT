25dd9f2e6eeb58f1612bbe181c04671a76443498
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-17 16:03:21 +0530

    made responsive

--- Server/index.js
+++ Server/index.js
@@ -14,24 +14,18 @@ const server = http.createServer(app)
 const io = socketio(server, { wsEngine: 'ws' })
 
 io.on('connection', (socket) => {
-    // console.log('We have a new connection!')
 
     socket.on('join', ({ name, room }, callback) => {
-        // console.log(name)
         const user = addUser({ id: socket.id, name: name, room: room });
         if (user.error) return callback(user.error)
-        // console.log(room)
-        // console.log(name)
-        // console.log(user)
         socket.join(user.room)
 
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room` })
         socket.broadcast.to(room).emit('message', { user: 'admin', text: `${user.name} has joined  ` })
 
-        // console.log(user.room)
         const users = getUsers()
         const usersInRoom = getUsersInRoom(users, user.room)
-        // console.log(usersInRoom)
+
         io.to(user.room).emit('roomData', { room: user.room, users: usersInRoom })
         callback()
     })
